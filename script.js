60d6ffffe7333edb190c005008288723c9224e3c
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-14 13:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function insert_Row() {
     let btn = document.querySelector('input[type="button"]');
     btn.addEventListener("click",function() {
         let tab = document.getElementById("sampleTable");
-		tab.deleteRow(0);
         let row = tab.insertRow(0);
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
