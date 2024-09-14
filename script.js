6f91a3ffaa029f1ccb013b734c7edb2ad649a4a5
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-14 13:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-window.onload = function() {
-    function insert_Row() {
-        let btn = document.querySelector('input[type="button"]');
-        btn.addEventListener("click",function() {
-            let tab = document.getElementById("sampleTable");
-            let row = tab.insertRow(0);
-            let cell1 = row.insertCell(0);
-            let cell2 = row.insertCell(1);
-            cell1.innerHTML = "NEW CELL1";
-            cell2.innerHTML = "NEW CELL2";
-        });
-    }
-    insert_Row(); // Call the function here
+function insert_Row() {
+    let btn = document.querySelector('input[type="button"]');
+    btn.addEventListener("click",function() {
+        let tab = document.getElementById("sampleTable");
+        let row = tab.insertRow(0);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        cell1.innerHTML = "NEW CELL1";
+        cell2.innerHTML = "NEW CELL2";
+    });
 }
 
+window.onload = function() {
+    insert_Row(); 
+}
